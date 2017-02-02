f78e68697b8186aa4c5d1f029c95f24dd3a1b9d3
Umar Niazi <uhkniazi@gmail.com>
2017-02-02 13:21:56 +0000

    GSEA analysis

---
+++ 12_gsea.R
@@ -0,0 +1,152 @@
+# File: 12_gsea.R
+# Auth: umar.niazi@kcl.ac.uk
+# Date: 02/02/2017
+# Desc: gene set enrichment analysis for the datasets
+
+## set variables and source libraries
+source('header.R')
+## libraries to load
+library(gage)
+
+## load gene expression data
+dfContrast = read.csv(file='Results/DEAnalysis_CLP_SI.vs.Media_3.xls', stringsAsFactors = F)
+dfContrast = dfContrast[,-1]
+
+## load msig db data
+oMsigGS = readList(file.choose())
+## OR try other datasets
+# library(gageData)
+# data("kegg.sets.hs")
+# oMsigGS = kegg.sets.hs
+## GO
+# data(go.sets.hs)
+# data(go.subs.hs)
+# names(go.subs.hs)
+# oMsigGS=go.sets.hs[go.subs.hs$MF]
+# oMsigGS=go.sets.hs[go.subs.hs$BP]
+# oMsigGS=go.sets.hs[go.subs.hs$CC]
+
+#dfContrast = dfContrast4
+# for a contrats of choice create the list
+iContFc = dfContrast$logFC
+names(iContFc) = as.character(dfContrast[,1])
+head(iContFc)
+head(dfContrast)
+
+oGage = gage(iContFc, oMsigGS)
+
+dfGreater = data.frame(oGage$greater)
+str(dfGreater)
+i = which(dfGreater$p.val < 0.01)
+rownames(dfGreater[i,])
+
+dfLess = data.frame(oGage$less)
+str(dfLess)
+i = which(dfLess$p.val < 0.01)
+rownames(dfLess[i,])
+
+write.csv(dfGreater[,c('p.val', 'q.val', 'set.size')], file='Results/upregulated_pathways_msigdb_c5.xls')
+write.csv(dfLess[,c('p.val', 'q.val', 'set.size')], file='Results/downregulated_pathways_msigdb_c5.xls')
+
+
+
+## make a barplot
+plot.bar = function(ivBar, title='', ...){
+  p.old = par(mar=c(8,4,2,2)+0.1)
+  l = barplot(ivBar, beside=T, xaxt='n', main=title, ...)
+  axis(side = 1, l[,1], labels=F)
+  text(l[,1], y=par()$usr[3]-0.1*(par()$usr[4]-par()$usr[3]),
+       labels=names(ivBar), srt=45, adj=1, xpd=TRUE, cex=0.6)
+  par(p.old)
+}
+
+dfBar = dfGreater[order(dfGreater$p.val), ]
+dfBar = dfLess[order(dfLess$p.val), ]
+
+i = -1*log10(dfBar$p.val[1:7])
+names(i) = rownames(dfBar)[1:7]
+
+plot.bar(i, title='Contrast 4 - down regulated GO BP', ylab='-log10 PValue')
+
+# #### test with kegg pathways data
+# library(pathview)
+# dir.create('Results/Kegg_figures')
+# pv.out.list <- sapply(substr(rownames(dfBar)[1], 1, 8), function(pid) pathview(
+#   gene.data = iContFc, pathway.id = pid,
+#   species = "hsa", out.suffix='Contrast 1 Down', kegg.dir = 'Results/Kegg_figures/'))
+# 
+# 
+# ### protein enrichment analysis using interaction database 
+# # ## some enrichment using string db package
+# library(STRINGdb)
+# # find database id for human
+# temp = get_STRING_species(version = '10')
+# temp[grep('homo', temp$official_name, ignore.case = T, perl=TRUE),]
+# 
+# dbString = STRINGdb$new(version='10', species=9606, input_directory='')
+# 
+# df.rn = dfContrast4
+# str(df.rn)
+# df.rn$ENTREZ.ID = as.character(df.rn$ENTREZ.ID)
+# rownames(df.rn) = df.rn$ENTREZ.ID
+# head(df.rn)
+# 
+# string.mapped = dbString$map(df.rn, 'ENTREZ.ID', removeUnmappedRows = T)
+# 
+# string.mapped = string.mapped[order(string.mapped$p.value),]
+# head(string.mapped)
+# 
+# # plot enrichment
+# dbString$plot_ppi_enrichment(string.mapped$STRING_id, quiet = T, title = 'Contrast 4 All')
+# dbString$plot_ppi_enrichment(string.mapped$STRING_id[1:700], quiet = T, title = 'Contrast 4')
+# #ig = dbString$get_graph()
+# #enrichment = ppi_enrichment_full(string.mapped$STRING_id, ig)
+# #enrichment = ppi_enrichment(string.mapped$STRING_id, ig)
+# #enrichment = dbString$get_ppi_enrichment(string.mapped$STRING_id[1:1000])
+# par(p.old)
+# hits = string.mapped$STRING_id[250:430]
+# #dbString$plot_network(hits)
+# pdf('Temp/string_contrast4_250-430.pdf')
+# dbString$plot_network(hits)
+# dev.off(dev.cur())
+# 
+# 
+# # category for which to compute the enrichment (i.e. "Process", "Component",
+# # "Function", "KEGG", "Pfam", "InterPro"). The default category is "Process".
+# enrichmentProcess = dbString$get_enrichment(string.mapped$STRING_id[1:500], category = 'Process', iea=F)
+# enrichmentKEGG = dbString$get_enrichment(string.mapped$STRING_id[1:500], category = 'KEGG', iea=F)
+# 
+# ## make bar plots and save data
+# write.csv(enrichmentProcess, file='Results/Keloid:2vsControl:2_protein_pathways_GO_BP.xls')
+# write.csv(enrichmentKEGG, file='Results/Keloid:2vsControl:2_protein_pathways_KEGG.xls')
+# 
+# dfBar = enrichmentProcess[order(enrichmentProcess$pvalue), ]
+# dfBar = enrichmentKEGG[order(enrichmentKEGG$pvalue), ]
+# 
+# i = -1*log10(dfBar$pvalue[1:7])
+# names(i) = dfBar$term_description[1:7]
+# 
+# plot.bar(i, title='Contrast 4 - Proteins KEGG', ylab='-log10 PValue')
+# 
+# 
+# # #################### compare 2 contrasts
+# # df.rn = dfContrast4
+# # str(df.rn)
+# # df.rn$ENTREZ.ID = as.character(df.rn$ENTREZ.ID)
+# # rownames(df.rn) = df.rn$ENTREZ.ID
+# # head(df.rn)
+# # 
+# # string.mapped = dbString$map(df.rn, 'ENTREZ.ID', removeUnmappedRows = T)
+# # 
+# # string.mapped = string.mapped[order(string.mapped$p.value),]
+# # 
+# # string.mapped.1 = string.mapped
+# # string.mapped.2 = string.mapped
+# # 
+# # hits.contrast1 = string.mapped.1$STRING_id[1:600]
+# # hits.contrast3 = string.mapped.2$STRING_id[1:600]
+# # 
+# # eh = dbString$enrichment_heatmap(list(hits.contrast1, hits.contrast3), vectorNames = list('Contrast1', 'Contrast3'), output_file = 'Temp/hm.pdf',
+# #                                  enrichmentType = 'Process',
+# #                                 title = 'Contrats 1 and Contrast 2 GO BP')
+# 
---
+++ 13_gsea_summary_tables.R
@@ -0,0 +1,134 @@
+# File: 13_gsea_summary_tables.R
+# Auth: umar.niazi@kcl.ac.uk
+# Date: 02/02/2017
+# Desc: summary tables after merging gsea data
+
+## set variables and source libraries
+source('header.R')
+
+lFiles = list.files('Results/', pattern='*mSigDb_c5.xls', full.names = T, ignore.case = T)
+
+# load the files
+ldfData = lapply(lFiles, function(x) as.data.frame(read.csv(x, row.names=1)))
+names(ldfData) = lFiles
+sapply(ldfData, nrow)
+# put everything in one order by row names
+rn = rownames(ldfData[[1]])
+head(rn)
+
+ldfData = lapply(ldfData, function(df){
+  df = df[rn,]
+})
+
+# get the upregulated/downregulated in order
+ldfData.up = ldfData[grepl(lFiles, pattern = 'upregulated')]
+ldfData.down = ldfData[grepl(lFiles, pattern = 'downregulated')]
+
+## set the names for each contrast
+sn = c('CLP_SI_vs_Media') 
+names(ldfData.up) = sn
+
+sn = c('CLP_SI_vs_Media') 
+names(ldfData.down) = sn
+
+## create a table/matrix of p-values
+mMerged.up = sapply(ldfData.up, function(x) x$p.val)
+rownames(mMerged.up) = rownames(ldfData.up[[1]])
+## create similar table for downregulated
+mMerged.down = sapply(ldfData.down, function(x) x$p.val)
+rownames(mMerged.down) = rownames(ldfData.down[[1]])
+
+# sanity check
+identical(rownames(mMerged.up), rownames(mMerged.down))
+cn = c('CLP_SI_vs_Media') 
+colnames(mMerged.up) = paste(cn, 'up', sep='-')
+colnames(mMerged.down) = paste(cn, 'down', sep='-')
+
+mMerged.c2 = cbind(mMerged.up, mMerged.down)
+
+# remove na sections
+dim(mMerged.c2)
+mMerged.c2 = na.omit(mMerged.c2)
+dim(mMerged.c2)
+head(mMerged.c2)
+
+### create a binary matrix based on cutoffs
+getBinaryMatrix = function(mat, cutoff=0.01){
+  mat2 = apply(mat, 2, function(x) round(x, 3) <= cutoff)
+}
+
+mMerged.c2.bin = getBinaryMatrix(mMerged.c2)
+
+## group this matrix into combinations
+mMerged.c2.bin.grp = mMerged.c2.bin
+set.seed(123)
+dm = dist(mMerged.c2.bin.grp, method='binary')
+hc = hclust(dm)
+plot(hc, labels=F)
+# cut the tree at the bottom to create groups
+cp = cutree(hc, h = 0.2)
+# sanity checks
+table(cp)
+length(cp)
+length(unique(cp))
+
+mMerged.c2.bin.grp = cbind(mMerged.c2.bin.grp, cp)
+
+### print and observe this table and select the groups you are interested in
+temp = mMerged.c2.bin.grp
+temp = (temp[!duplicated(cp),])
+temp2 = cbind(temp, table(cp))
+rownames(temp2) = NULL
+print(temp2)
+
+## map these names to the cp
+groups = cp
+sig.pvals = rowSums(mMerged.c2.bin)
+dfMerged.c2 = data.frame(round(mMerged.c2, 3), sig.pvals, groups, DB='C5-Mouse')
+str(dfMerged.c2)
+head(dfMerged.c2)
+tail(dfMerged.c2)
+
+#write.csv(dfMerged.c2, file='Results/gsea_msigdb_c2_merged.xls')
+write.csv(dfMerged.c2, file='Results/gsea_msigdb_c5_merged.xls')
+
+# ## merge together into one dataframe
+# table(dfMerged.c2$groups)
+# dfMerged.c2.sub = dfMerged.c2[dfMerged.c2$groups != 3,]
+# 
+# table(dfMerged.c5$groups)
+# dfMerged.c5.sub = dfMerged.c5[dfMerged.c5$groups != 2,]
+# 
+# dfMerged = rbind(dfMerged.c2.sub, dfMerged.c5.sub)
+# dfMerged = droplevels.data.frame(dfMerged)
+# 
+# write.csv(dfMerged, file='Results/gsea_msigdb_significant_c2_c5_merged.csv')
+
+### heatmaps
+dfMerged = dfMerged.c2
+df = dfMerged
+head(df)
+mMat = as.matrix(df[,c(1:2)])
+head(mMat)
+mMat = -10*log10(mMat+1e-16)
+g1 = df[,'groups']
+g1 = factor(as.character(g1))
+levels(g1)
+g2 = df[,'DB']
+g2 = factor(as.character(g2))
+levels(g2)
+
+ann = data.frame(DB=g2, Group=g1 )
+range(mMat)
+mMat[mMat < 15] = 0 
+mMat[mMat > 100] = 100
+
+library(NMF)
+library(RColorBrewer)
+aheatmap(mMat, annRow = ann, scale = 'none', Rowv = order(g2:g1), Colv=NA, cexRow=5, cexCol = 0.6, #labCol=c('C2vC1', 'K1vC1', 'K2vC2', 'K2vK1'), 
+         col=c('white', brewer.pal(9, 'YlOrRd')))
+
+pdf('Results/gsea_msigdb_significant_merged.pdf')
+aheatmap(mMat, annRow = ann, scale = 'none', Rowv = order(g2:g1), Colv=NA, cexRow=5, cexCol = 0.6, #labCol=c('C2vC1', 'K1vC1', 'K2vC2', 'K2vK1'), 
+         col=c('white', brewer.pal(9, 'YlOrRd')))
+dev.off(dev.cur())
